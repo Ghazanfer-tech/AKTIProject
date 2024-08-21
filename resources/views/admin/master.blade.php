9856e19abb18535faa102ca4a8aeb03b159f8084
Ghazanfer-tech <aghazanfer730@gmail.com>
2024-08-21 04:48:29 -0700

    new route add in admin web button

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -182,6 +182,8 @@
                                 style="width: 40px; height: 40px;">
                             <span class="d-none d-lg-inline-flex">Ghazanfar</span>
                         </a>
+                        <a class="btn border-secondary rounded-pill px-2 py-2 text-primary text-uppercase mb-2 ms-2"
+                        type="button" href= "{{ route('web-shop') }}">web </a>
                         {{-- <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                             <a href="#" class="dropdown-item">My Profile</a>
                             <a href="#" class="dropdown-item">Settings</a>
